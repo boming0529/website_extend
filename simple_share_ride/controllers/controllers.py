b1fa7a3e1429611a33796273361fc9f0dd9b9f1f
boming <boming0529@gmail.com>
2018-11-19 22:56:29 +0800

    [Fix] simple_share_ride : create sale order

--- simple_share_ride/controllers/controllers.py
+++ simple_share_ride/controllers/controllers.py
@@ -40,5 +40,79 @@ class share_ride(http.Controller):
             })],
             'user_id': False,
         })
-        new_so.force_quotation_send()
+        # new_so.force_quotation_send()
         return request.redirect('/page/homepage')
+
+    # @http.route(['/share/quotes', '/share/quotes/page/<int:page>'], type='http', method=["Get"], auth="user", website=True)
+    # def share_quotes(self, page=1, date_begin=None, date_end=None, **kw):
+    #     values = {}
+    #     driver = request.env.user.partner_id
+    #     sale_order = request.env['sale.order']
+
+    #     domain = [
+    #         ('user_id', '=', False),
+    #         ('state', '=', 'draft'),
+    #     ]
+
+    #     if date_begin and date_end:
+    #         domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+    #     # count for pager
+    #     quotation_count = sale_order.search_count(domain)
+    #     # make pager
+    #     pager = request.website.pager(
+    #         url="/share/quotes",
+    #         url_args={'date_begin': date_begin, 'date_end': date_end},
+    #         total=quotation_count,
+    #         page=page,
+    #         step=20
+    #     )
+    #     # search the count to display, according to the pager data
+    #     quotations = sale_order.search(domain, limit=20, offset=pager['offset'])
+
+    #     values.update({
+    #         'date': date_begin,
+    #         'quotations': quotations,
+    #         'pager': pager,
+    #         'default_url': '/share/quotes',
+    #         'driver': driver,
+    #     })
+    #     return request.render("simple_share_ride.diver_quotation_list", values)
+
+    # @http.route(['/share/quotes_sent'], type='http', method=["Post"], auth="user", website=True, csrf=False)
+    # def portal_my_quotes(self, **post):
+    #     values = {}
+    #     driver = request.env.user.partner_id
+    #     sale_order = request.env['sale.order']
+    #     new_so.force_quotation_send()
+    #     domain = [
+    #         ('user_id', '=', False),
+    #         ('state', '=', 'draft'),
+    #     ]
+
+    #     if date_begin and date_end:
+    #         domain += [('create_date', '>', date_begin),
+    #                    ('create_date', '<=', date_end)]
+
+    #     # count for pager
+    #     quotation_count = sale_order.search_count(domain)
+    #     # make pager
+    #     pager = request.website.pager(
+    #         url="/share/quotes",
+    #         url_args={'date_begin': date_begin, 'date_end': date_end},
+    #         total=quotation_count,
+    #         page=page,
+    #         step=20
+    #     )
+    #     # search the count to display, according to the pager data
+    #     quotations = sale_order.search(
+    #         domain, limit=20, offset=pager['offset'])
+
+    #     values.update({
+    #         'date': date_begin,
+    #         'quotations': quotations,
+    #         'pager': pager,
+    #         'default_url': '/share/quotes',
+    #         'driver': driver,
+    #     })
+    #     return request.render("simple_share_ride.diver_quotation_list", values)
