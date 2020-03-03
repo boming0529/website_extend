1521146e89a47cf013d7ad933b105763897b192f
boming <boming0529@gmail.com>
2020-03-03 16:07:12 +0800

    [Fix] website_widget_demo : impovement and remove not using code

--- website_widget_demo/controllers/controllers.py
+++ website_widget_demo/controllers/controllers.py
@@ -6,20 +6,7 @@ class WebsiteWidgetDemo(http.Controller):
 
     @http.route('/demo/index', type="http", auth='public', method=["Get"], website=True)
     def demo_indexs(self, **kw):
-        return http.request.render('website_widget_demo.index', {
-            'heroes':  [
-                {'id': 11, 'name': 'Mr. Nice'},
-                {'id': 12, 'name': 'Narco'},
-                {'id': 13, 'name': 'Bombasto'},
-                {'id': 14, 'name': 'Celeritas'},
-                {'id': 15, 'name': 'Magneta'},
-                {'id': 16, 'name': 'RubberMan'},
-                {'id': 17, 'name': 'Dynama'},
-                {'id': 18, 'name': 'Dr IQ'},
-                {'id': 19, 'name': 'Magma'},
-                {'id': 20, 'name': 'Tornado'}
-            ]
-        })
+        return http.request.render('website_widget_demo.index',{})
 
     @http.route('/get/heroes/', type="json", auth='public', website=True, method=['POST'], csrf=False)
     def get_heroes(self, **kw):
